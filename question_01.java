f76c496396c1979b4ca3ae87f949c9a0d63ca962
Thanushangit <sinnarasathanushan2002@gmail.com>
2024-04-28 16:23:12 +0530

    arithmetic operations

--- question_01.java
+++ question_01.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 public class question_01 {
     public static void main(String[]args){
-        Scanner in=new Scanner(System.in);
+        Scanner in =new Scanner(System.in);
         String title;
         String auname;
         int year;
         int price;
 
-        System.out.println("Enter the book title");
+        System.out.print("Enter the book title:");
         title=in.nextLine();
-        System.out.println("Enter the Author name");
+        System.out.print("Enter the Author name:");
         auname=in.nextLine();
-        System.out.println("Enter the publication year");
+        System.out.print("Enter the publication year:");
         year=in.nextInt();
-        System.out.println("Enter the Price");
+        System.out.print("Enter the Price:");
         price=in.nextInt();
 
         System.out.println("Book details:");
---
+++ question_02.java
@@ -0,0 +1,15 @@
+public class question_02 {
+    public static void main(String[]args){
+        int ans1=3+4*5;
+        int ans2=5+12-3*2;
+        int ans3=(8+4)-(6+2);
+        int ans4=10+15/3;
+        double ans5=12.5/(3.0-1.5);
+
+        System.out.println("3+4*5= "+ ans1);
+        System.out.println("5+12-3*2= "+ ans2);
+        System.out.println("(8+4)-(6+2)= "+ ans3);
+        System.out.println("10+15/3= "+ ans4);
+        System.out.println("12.5/(3.0-1.5)= "+ ans5);
+    }
+}
